ee64a1dabd2f550007caf8bd2426d415ad370f45
h-hyakut <hocha01n@gmail.com>
2024-06-07 11:18:39 +0900

    07-login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resources)
+    mypage_path
+  end
+
+  def after_sign_out_path_for(resources)
+    new_user_session_path
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get :mypage, to: "mypage#index"
+  
   resources :taggings
   resources :books
   resources :tags
