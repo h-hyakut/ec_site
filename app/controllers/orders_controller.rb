61f6d0387d2d69179d7735747063f16d09a3cd15
h-hyakut <hocha01n@gmail.com>
2024-06-25 11:56:10 +0900

    sold_out, cart updata完了！

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,13 +3,19 @@ class OrdersController < ApplicationController
   def new
      @order = Order.new
      @books = @current_cart.books
+
+     sold_out_books = @current_cart.books.where(status: :sold_out)
+    if sold_out_books.exists?
+      redirect_to products_path
+      return
+    end
   end
     
   def confirm
     @count = order_params[:count]
     @order = Order.new(order_params)
     # @book = Book.find(@order.book_id) #new アクションで設定された @book 変数
-    @books = Book.where(order_params[:book_ids]) #new アクションで設定された @book 変数
+    @books = Book.where(id: order_params[:book_ids]) #new アクションで設定された @book 変数
     @line_items = @current_cart.line_items
 
     order_params[:count].each_with_index do |count, index|
@@ -20,29 +26,30 @@ class OrdersController < ApplicationController
         line_item.update(quantity: count)
       end
     end
-
-    @books.each do |book|
-      if book.sold_out?
-        redirect_to product_path(book)
-        flash[:alert] = "This item is currently sold out and unavailable for purchase...🙇"
-      return
-      end
-    end
   end
+
+  #   @books.each do |book|
+  #     if book.sold_out?
+  #       redirect_to product_path(book)
+  #       flash[:alert] = "This item is currently sold out and unavailable for purchase...🙇"
+  #     return
+  #     end
+  #   end
+  # end
 
   def create
     count = order_params[:count].map(&:to_i).sum
     @order = Order.new(address: order_params[:address], count: count)
-    @books = Book.where(id: order_params[:book_id])
+    @books = Book.where(id: order_params[:book_ids])
+    @line_items = @current_cart.line_items
+
     if @order.save
-      if @books.present?
-        @books.each {|book| book.sold_out!}
-      else
-        render "confirm" and return # 注文が保存できなかった場合は確認画面を再表示
-      end
+      OrderDetail.create_items(@order, @line_items)
       
-      OrderDetail.create_items(@order, @current_cart.line_items)
-      redirect_to complete_orders_path(@order), notice: "Order was successfully placed!"
+      @line_items.each do |item|
+        item.book.sold_out!
+      end
+        redirect_to complete_orders_path(@order), notice: "Order was successfully placed!"
     else
       render "confirm"
     end
@@ -50,15 +57,14 @@ class OrdersController < ApplicationController
 
 
   def complete
-    @orders = Order.where(params[:id])
+    @orders = Order.where(id: params[:id])
     CompleteMailer.complete_mail(current_user).deliver
-    
   end
 
   # order/new...book_id=1 で飛ぶときに　@bookをリンクに入れることで詳細画面に戻り、自然にページを移るようにできた
   private
 
   def order_params
-    params.require(:order).permit(:address, count: [], book_ids: [])
+    params.require(:order).permit(:status, :address, count: [], book_ids: [])
   end
 end
